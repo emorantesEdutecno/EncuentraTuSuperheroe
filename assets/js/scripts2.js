4b149f2646d0a5fdb79db80270f6dcb2bde3c48b
Didier Morantes <emorantes@edutecno.com>
2022-05-12 12:47:20 -0500

    correccion en error de imagenes no existentes

--- assets/js/scripts2.js
+++ assets/js/scripts2.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  //rama master actualizada
+
   var laImagen = '';
 
   $('#imagenHeroe').on('error', function(){
@@ -10,6 +10,7 @@ $(document).ready(function(){
   });
 
 
+  //rama master actualizada
   $('#formHeroe').submit(function(event){
     event.preventDefault();
     console.log('funciona submit');
@@ -25,9 +26,9 @@ $(document).ready(function(){
       success: function(data){
             $('#mensajeHeroe').text('SuperHeroe Encontrado');
             console.log(data);
-
             laImagen = data.image.url;
-            // $('#imagenHeroe').attr("src", `${data.image.url}`);
+
+            console.log(`ruta imagen: ${laImagen}`);
             $('#imagenHeroe').attr("src", laImagen );
 
             let elNombre = data.name;
@@ -47,34 +48,41 @@ $(document).ready(function(){
             let laFuerza = data['powerstats']['strength'] == 'null' ? 0 : parseInt(data['powerstats']['strength']);
             let laVelocidad = data['powerstats']['speed'] == 'null' ? 0 : parseInt(data['powerstats']['speed']);
 
-            var chart = new CanvasJS.Chart("chartContainer", {
-                        	theme: "light2", // "light1", "light2", "dark1", "dark2"
-                        	exportEnabled: true,
-                        	animationEnabled: true,
-                        	title: {
-                        		text: `Estadisticas de poder para ${elNombre}`,
-                        	},
-                        	data: [{
-                        		type: "pie",
-                        		startAngle: 25,
-                        		toolTipContent: "<b>{label}</b>: {y}",
-                        		showInLegend: "true",
-                        		legendText: "{label}",
-                        		indexLabelFontSize: 16,
-                        		indexLabel: "{label} - {y}",
-                        		dataPoints: [
-                        			{ y: laInteligencia, label: "Intelligence" },
-                        			{ y: laFuerza, label: "Strength" },
-                        			{ y: laVelocidad, label: "Speed" },
-                              //FALTA ACTUALIZAR LOS DEMÁS PARAMETROS DE ACUERDO A LA RESPUESTA DEL API
-                        			{ y: 5.02, label: "Microsoft Edge" },
-                        			{ y: 4.07, label: "Safari" },
-                        			{ y: 1.22, label: "Opera" },
-                        			{ y: 0.44, label: "Others" }
-                        		]
-                        	}]
-                        });
-                        chart.render();
+
+
+          var chart = new CanvasJS.Chart("chartContainer", {
+          	animationEnabled: true,
+
+          	title:{
+          		text: `Estadisticas de poder para ${elNombre}`,
+          	},
+          	axisX:{
+          		interval: 1
+          	},
+          	axisY2:{
+          		interlacedColor: "rgba(1,77,101,.2)",
+          		gridColor: "rgba(1,77,101,.1)",
+          		title: "Estadisticas"
+          	},
+          	data: [{
+          		type: "bar",
+          		name: "companies",
+          		axisYType: "secondary",
+          		color: "#014D65",
+          		dataPoints: [
+                { y: laInteligencia, label: "Intelligence" },
+                { y: laFuerza, label: "Strength" },
+                { y: laVelocidad, label: "Speed" },
+                //FALTA ACTUALIZAR LOS DEMÁS PARAMETROS DE ACUERDO A LA RESPUESTA DEL API
+                { y: 5.02, label: "Microsoft Edge" },
+                { y: 4.07, label: "Safari" },
+                { y: 1.22, label: "Opera" },
+                { y: 0.44, label: "Others" }
+          		]
+          	}]
+          });
+          chart.render();
+
 
       },
       error: function(data){
